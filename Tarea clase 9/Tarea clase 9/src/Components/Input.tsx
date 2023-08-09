459f1b04ee96867518fd2f927d188cf4cae80d0b
Ornella <nelitagrigolato@gmail.com>
2023-08-09 18:49:54 -0300

    Colocando logica de mensaje en un useEffect

--- Tarea clase 9/Tarea clase 9/src/Components/Input.tsx
+++ Tarea clase 9/Tarea clase 9/src/Components/Input.tsx
@@ -1,20 +1,23 @@
 type proptypes = {
-    onChange: Function
-    value: string | undefined
-}
+  onChange: Function;
+  value: string | undefined;
+};
 
-function Input(props:proptypes) {
-
-    const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  
-        props.onChange(event.target?.value);        
-    }
-
-
+function Input(props: proptypes) {
+  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.onChange(event.target?.value);
+  };
 
   return (
-    <input className="p-[5px] m-[30px] border-2 border-gray-700 w-40 h-8"required type="number" value={props.value} placeholder="Ingresa tu número" onChange={(event) => inputChange(event)} ></input>
-  )
+    <input
+      className="p-[5px] m-[30px] border-2 border-gray-700 w-40 h-8"
+      required
+      type="number"
+      value={props.value}
+      placeholder="Ingresa tu número"
+      onChange={(event) => inputChange(event)}
+    ></input>
+  );
 }
 
-export default Input
+export default Input;
